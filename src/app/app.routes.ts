6bd85b7b7dfc238a5ff2789a8416dab048906cf2
Peter Gurský <peter.gursky@upjs.sk>
2026-03-03 13:07:27 +0100

    register komponent prva verzia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { UsersTable } from '../pages/users-table/users-table';
 import { Login } from '../pages/login/login';
 import { PageNotFound } from '../pages/page-not-found/page-not-found';
+import { Register } from '../pages/register/register';
 
 export const routes: Routes = [
   {path: 'users', component: UsersTable},
   {path: 'login', component: Login},
+  {path: 'register', component: Register},
   {path: '', redirectTo:'/login', pathMatch: 'full'},
   {path: '**', component: PageNotFound}
 ];
